7732e462545c18da60bd3e953f34b729249a02d1
Derek Truesdale <dtruesdale37@gmail.com>
2023-07-03 17:00:13 -0500

    lab completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayDirectors = directors.map(director => {
+    return <div key={director.name}>
+      {director.name}
+      {director.movies.map(movie=>{
+        return <li key={movie}>{movie}</li>
+      })}
+    </div>
+  })
+
+  return <div>
+      <h1>Directors Page</h1>
+      <div>{displayDirectors}</div>
+    </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(movies)
+  const displayMovies=movies.map(movie=>{
+    return <div key={movie.title}>{movie.title}, {movie.time}
+      <ul>
+        {movie.genres.map(genre=>{
+          return <li key={genre}>{genre}</li>
+        })}
+      </ul>
+    </div>
+    
+  })
+
+  return <div>
+    <h1>Movies Page</h1>
+    <div>{displayMovies}</div>
+  </div>;
 }
 
 export default Movies;
